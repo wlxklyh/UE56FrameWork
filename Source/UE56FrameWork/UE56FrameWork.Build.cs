b8791b79eeb119cc5e51cec5da83eb642d67cbe8
wlxklyh <linyanhou@qq.com>
2025-06-26 03:48:11 +0800

    【Puerts】官方demo config和依赖修改

--- Source/UE56FrameWork/UE56FrameWork.Build.cs
+++ Source/UE56FrameWork/UE56FrameWork.Build.cs
@@ -8,7 +8,7 @@ public class UE56FrameWork : ModuleRules
 	{
 		PCHUsage = PCHUsageMode.UseExplicitOrSharedPCHs;
 	
-		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore", "EnhancedInput" });
+		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore", "EnhancedInput" , "JsEnv", "UMG", "Puerts"});
 
 		PrivateDependencyModuleNames.AddRange(new string[] {  });
 
